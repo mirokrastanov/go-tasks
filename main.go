8823ce44d533c8363c0967f7eed373adadb2bcfc
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2025-02-10 17:52:10 +0200

    Define type. Add landing route and a create todo route.

--- main.go
+++ main.go
@@ -7,9 +7,41 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
 func main() {
 	fmt.Println("Hello, world!")
 	app := fiber.New()
+
+	todos := []Todo{}
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "Hello, world!"})
+	})
+
+	// Create a Todo item
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{} // initial values { id: 0, completed: false, body: ""}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Todo body is required"})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todo)
+	})
+
+	// Update a Todo item
 
 	log.Fatal(app.Listen(":4000"))
 }
