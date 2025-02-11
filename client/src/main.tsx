8c03da06a1b54e08be6ce491088d55623c22b834
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2025-02-11 18:25:51 +0200

    Set up project. Add assets & dependencies. Base config.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,13 @@
-import { Provider } from "@/components/ui/provider"
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
+import './index.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
+    <ChakraProvider>
       <App />
-    </Provider>
+    </ChakraProvider>
   </StrictMode>,
 )
