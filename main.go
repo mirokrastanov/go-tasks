74d9a7e3b25333381e4406c273dc857f33d96a37
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2025-02-11 15:44:47 +0200

    Add a get all todos route and handler

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -50,10 +51,35 @@ func main() {
 
 	app := fiber.New()
 
+	app.Get("/api/todos", getAllTodos)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "4000"
 	}
 
 	log.Fatal(app.Listen("0.0.0.0:" + port))
+}
+
+func getAllTodos(c *fiber.Ctx) error {
+	var todos []Todo
+
+	cursor, err := collection.Find(context.Background(), bson.M{})
+
+	if err != nil {
+		return err
+	}
+
+	defer cursor.Close(context.Background())
+
+	for cursor.Next(context.Background()) {
+		var todo Todo
+		err := cursor.Decode(&todo)
+		if err != nil {
+			return err
+		}
+		todos = append(todos, todo)
+	}
+
+	return c.JSON(todos)
 }
