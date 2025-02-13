81ee3fb0eba123901dcd43c033050c4a0dbdd5a8
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2025-02-13 11:35:17 +0200

    Add Todo List

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Container, Stack } from "@chakra-ui/react"
 import Navbar from "./components/Navbar"
 import TodoForm from "./components/TodoForm"
+import TodoList from "./components/TodoList"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Container>
         <TodoForm />
+        <TodoList />
       </Container>
     </Stack>
   )
