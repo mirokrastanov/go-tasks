e1d76a3aadbaef0e276e59d10f61bc9e95e1165b
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2025-02-13 13:17:54 +0200

    Implement delete todo and update in real time

--- client/src/components/TodoItem.tsx
+++ client/src/components/TodoItem.tsx
@@ -34,9 +34,28 @@ const TodoItem = ({ todo }: { todo: Todo }) => {
 		},
 	});
 
-	// useEffect(() => {
-	// 	console.log(BE_BASE_URL);
-	// }, []);
+	const { mutate: deleteTodo, isPending: isDeleting } = useMutation({
+		mutationKey: ['deleteTodo'],
+		mutationFn: async () => {
+			await new Promise(resolve => setTimeout(resolve, 500)); //sim loading delay
+
+			try {
+				const res = await fetch(`${BACKEND_URL}/todos/${todo._id}`, {
+					method: 'DELETE',
+				});
+				const data = await res.json();
+				if (!res.ok) {
+					throw new Error(data.error || 'Failed to delete todo');
+				}
+				return data;
+			} catch (error) {
+				console.log(error);
+			}
+		},
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['todos'] });
+		},
+	});
 
 	return (
 		<Flex gap={2} alignItems={"center"}>
@@ -71,8 +90,9 @@ const TodoItem = ({ todo }: { todo: Todo }) => {
 					{!isUpdating && <FaCheckCircle size={20} />}
 					{isUpdating && <Spinner size={'sm'} />}
 				</Box>
-				<Box color={"red.500"} cursor={"pointer"}>
-					<MdDelete size={25} />
+				<Box color={"red.500"} cursor={"pointer"} onClick={() => deleteTodo()}>
+					{!isDeleting && <MdDelete size={25} />}
+					{isDeleting && <Spinner size={'sm'} />}
 				</Box>
 			</Flex>
 		</Flex>
