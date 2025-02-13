1c1be54b29462b659fc9050653b004d772ff1a37
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2025-02-13 11:39:50 +0200

    Add TanStack Query and wrap the app with it

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,18 @@
-import { StrictMode } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
-import './index.css'
 import App from './App.tsx'
+import './index.css'
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
