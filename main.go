f5275471e69cf3cd32cbdb254c4baefd00b097fb
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2025-02-13 11:52:23 +0200

    Add & set up CORS middleware to server

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -50,6 +51,12 @@ func main() {
 	collection = client.Database("golang_db").Collection("todos")
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		// AllowOrigins: "*",
+		AllowOrigins: "http://localhost:5173",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	// Routes
 	app.Get("/api/todos", getAllTodos)
